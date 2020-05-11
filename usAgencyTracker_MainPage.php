5d483ca26be70963e06ae1d095e4fa8f90c1d86b
xtonx <jm@xt723.org>
2020-05-11 14:13:23 -0700

    Created usAgencytracker_MainPage.php, added Head and Header.

---
+++ usAgencyTracker_MainPage.php
@@ -0,0 +1,15 @@
+<head>
+  <title>US Agencies Tracker</title>
+
+  <link rel="stylesheet" href="">
+  <link href="https://fonts.googleapis.com/css?family=Nunito+Sans&display=swap" rel="stylesheet">
+  <?php require 'phpMysqlUtility.php'; ?>
+
+</head>
+
+<header>
+  <nav class= 'top-of-page'>
+      <a class="active" href="#home">US Agencies Tracker</a>
+      <input class= 'input-search' type='search' placeholder='Search'>
+  </nav>
+</header>
